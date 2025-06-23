e0787567dacc0fe06c96bc46695901474c73bd78
Ashish Arya <arya.ashish01@yahoo.com>
2025-06-22 22:37:36 -0400

    initial commit

---
+++ src/test/java/test/BaseTest.java
@@ -0,0 +1,24 @@
+package test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+public class BaseTest {
+
+	public static WebDriver driver; 
+	
+	@BeforeTest
+	public  void setup()
+	{
+		driver = new ChromeDriver();
+		driver.get("https://www.facebook.com");
+		
+	}
+	@AfterTest
+	public void tearDown()
+	{
+		driver.quit();
+	}
+}
---
+++ src/test/java/test/BasicValidation.java
@@ -0,0 +1,44 @@
+package test;
+
+import static org.testng.Assert.assertTrue;
+
+import org.openqa.selenium.By;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class BasicValidation extends BaseTest{
+    
+	@Test
+	
+	public void welcomeMessage() {
+		String expectedResult ="Connect with friends and the world around you on Facebook.";
+		String actualResult = driver.findElement(By.xpath("//h2[@class='_8eso']")).getText();
+		
+		Reporter.log("Expected Result =" + expectedResult);
+		Reporter.log("Actual Result =" + actualResult);
+		assertTrue(actualResult.equals(expectedResult), "Mismatch in the welcome message");
+		
+	}
+@Test
+	
+	public void emailValidation() {
+		String expectedResult ="Email or phone number";
+		String actualResult = driver.findElement(By.id("email")).getDomAttribute("placeholder");
+		
+		Reporter.log("Expected Result =" + expectedResult);
+		Reporter.log("Actual Result =" + actualResult);
+		assertTrue(actualResult.equals(expectedResult), "Mismatch in the email");
+		
+	}
+@Test
+
+public void passValidation() {
+	String expectedResult ="Password";
+	String actualResult = driver.findElement(By.id("pass")).getDomAttribute("placeholder");
+	
+	Reporter.log("Expected Result =" + expectedResult);
+	Reporter.log("Actual Result =" + actualResult);
+	assertTrue(actualResult.equals(expectedResult), "Mismatch in the Password");
+	
+}
+}
